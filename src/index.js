a3457211aba07228154394d1d0db3685d5d62a9a
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-07-02 01:38:58 +0300

    add basename to <BrowserRouter>

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { GlobalStyles, theme } from 'styles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/goit-react-hw-05-movies">
       <ThemeProvider theme={theme}>
         <Global styles={GlobalStyles} />
         <App />
